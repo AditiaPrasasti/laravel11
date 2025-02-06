30aa71df1822b464d6b6b637eb9441c5e34e37ff
Aditia <aditpraz222@gmail.com>
2025-02-06 20:25:46 +0700

    Refactor UI: Migrate homepage styles to Tailwind CSS

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,29 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    <link rel="stylesheet" href="css/page.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Halaman Contact</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="topnav">
-        <a href="/">Home</a>
-        <a href="/blog">Blog</a>
-        <a class="active" href="/contact">Contact</a>
-        <a href="/about">About</a>
-    </div>
-    <div class="header">
-        <h2>Contact</h2>
-        <div class="card">
-            <div class="container">
-                <h4><b>Aditia</b></h4>
-                <p>Front End</p>
-                <a href="#"><i class="fa fa-instagram"></i></a>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-            </div>
-        </div>
-    </div>
-</div>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <a href="/about">About</a>
+
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https://www.instagram.com/aditprzz/">Instagram</a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/aditprzz/">Youtube</a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/aditprzz/">Linkedin</a>
+        </li>
+    </ul>
 </body>
 </html>
