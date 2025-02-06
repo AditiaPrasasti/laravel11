534b4bc71a8637283f8174c2569df9982ed9497f
Aditia <aditpraz222@gmail.com>
2025-02-06 17:03:16 +0700

    feat: add homepage UI layout

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="css/page.css">
+</head>
+<body>
+    <div class="topnav">
+        <a href="/">Home</a>
+        <a class="active" href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+    </div>
+    <div class="header">
+        <h2>Blog Name</h2>
+    </div>
+
+    <div class="row">
+        <div class="leftcolumn">
+            <div class="card">
+                <h2>TITLE HEADING</h2>
+                <h5>Title description, Dec 7, 2017</h5>
+                <img class="image" src="img/LogoUmri.jpg" alt="Logo Umri" width="10%">
+                <p>Some text..</p>
+            </div>
+            <div class="card">
+                <h2>TITLE HEADING</h2>
+                <h5>Title description, Sep 2, 2017</h5>
+                <img class="image" src="img/LogoUmri.jpg" alt="Logo Umri" width="10%">
+                <p>Some text..</p>
+            </div>
+        </div>
+        <div class="rightcolumn">
+            <div class="card">
+                <h2>About Me</h2>
+                <img class="image" src="img/LogoUmri.jpg" alt="Logo Umri" width="10%">
+                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+            </div>
+            <div class="card">
+                <h3>Popular Post</h3>
+                <img class="image" src="img/LogoUmri.jpg" alt="Logo Umri" width="10%">
+                <img class="image" src="img/LogoUmri.jpg" alt="Logo Umri" width="10%">
+                <img class="image" src="img/LogoUmri.jpg" alt="Logo Umri" width="10%">
+            </div>
+            <div class="card">
+                <h3>Follow Me</h3>
+                <p>Some text..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+    <h2>Footer</h2>
+</div>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+    <link rel="stylesheet" href="css/page.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="topnav">
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a class="active" href="/contact">Contact</a>
+        <a href="/about">About</a>
+    </div>
+    <div class="header">
+        <h2>Contact</h2>
+        <div class="card">
+            <div class="container">
+                <h4><b>Aditia</b></h4>
+                <p>Front End</p>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
